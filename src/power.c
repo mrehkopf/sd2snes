72b9444861b6276caf910f33e89c19786e4c1681
ikari <ikari@elektra.micecat.ath.cx>
2011-02-08 00:59:36 +0100

    firmware: initial power management

--- src/power.c
+++ src/power.c
@@ -4,7 +4,23 @@
 #include "bits.h"
 #include "power.h"
 
-
+/*
+   required units:
+   * SSP0 (FPGA interface) [enabled via spi_init]
+   * UART3 (debug console) [enabled via uart_init]
+   * TIM3 (FPGA clock)
+   * RTC
+   * GPIO
+   * GPDMA [enabled via spi_init]
+   * USB [enabled via usb_init]
+   * PWM1
+*/
 void power_init() {
-  LPC_SC->PCONP |= (BV(PCTIM3) | BV(PCUART3));
+  LPC_SC->PCONP = BV(PCSSP0)
+                | BV(PCTIM3)
+                | BV(PCRTC)
+                | BV(PCGPIO)
+                | BV(PCPWM1)
+//                 | BV(PCUSB)
+  ;
 }
