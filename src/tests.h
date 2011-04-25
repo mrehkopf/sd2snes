747e04e01054c2b2b5a4fc50bc56ed87fc5e1d76
ikari <ikari@elektra.micecat.ath.cx>
2011-04-25 03:34:37 +0200

    firmware: add tests.h

---
+++ src/tests.h
@@ -0,0 +1,11 @@
+#ifndef _TESTS_H
+#define _TESTS_H
+
+void test_rom_writeloop(void);
+void test_dumprom(char *filename);
+void test_sdbench_local(char *filename);
+void test_rom_rw(void);
+void test_fpga_echo(void);
+
+
+#endif
