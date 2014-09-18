80d3613960c6f949bfead5d7faba813b3e4f47f1
ikari_01 <ikari@elektra.micecat.ath.cx>
2014-09-18 17:04:13 +0200

    Firmware: cheat deployment

---
+++ src/cheat.c
@@ -0,0 +1,98 @@
+#include "config.h"
+#include "fileops.h"
+#include "uart.h"
+#include "memory.h"
+#include "fpga_spi.h"
+#include "cheat.h"
+
+void cheat_program() {
+  cht_record_t cheat;
+  uint32_t cht_record_addr = SRAM_CHEAT_ADDR + 1;
+  uint8_t cht_count;
+  uint8_t cht_index, rom_index = 0, wram_index = 0;
+  uint8_t is_wram_cheat, is_disabled;
+  uint32_t patch_addr;
+  uint8_t enable_mask = 0;
+
+  cht_count = sram_readbyte(SRAM_CHEAT_ADDR);
+cht_count = 0;
+  printf("cheat_program: %d cheats present\n", cht_count);
+  /* get list of activated cheats from menu */
+  for(cht_index = 0; cht_index < cht_count; cht_index++) {
+    sram_readblock(&cheat, cht_record_addr, sizeof(cht_record_t));
+  /* determine ROM or WRAM cheat */
+    patch_addr = ((uint32_t)cheat.patchbank << 16) | cheat.patchaddr;
+    is_wram_cheat = ((patch_addr & 0xfe0000) == 0x7e0000)
+                    || (!(patch_addr & 0x400000)
+                        && ((patch_addr & 0xffff) < 0x2000));
+    is_disabled = cheat.flags & 0x04;
+  /* apply cheat to FPGA / NMI hook */
+    if(!is_disabled) {
+      if(is_wram_cheat) {
+        cheat_program_ram_cheat(wram_index++, &cheat);
+      } else if(rom_index < 6){
+        enable_mask |= (1 << rom_index);
+        cheat_program_rom_cheat(rom_index++, &cheat);
+      }
+    }
+    cht_record_addr += sizeof(cht_record_t);
+  }
+  /* put number of WRAM cheats + enable flag */
+  fpga_set_snescmd_addr(0xbf);
+  fpga_write_snescmd(wram_index);
+  printf("enable mask=%02x\n", enable_mask);
+  fpga_write_cheat(6, enable_mask);
+  cheat_enable(1);
+  cheat_nmi_enable(1);
+}
+
+void cheat_program_rom_cheat(int index, cht_record_t *cheat) {
+  uint32_t code = ((uint32_t)cheat->patchbank << 24)
+                  | ((uint32_t)cheat->patchaddr << 8)
+                  | cheat->patchvalue;
+  printf("ROM cheat #%d: %02x%04x %02x\n", index, cheat->patchbank, cheat->patchaddr, cheat->patchvalue);
+  fpga_write_cheat(index, code);
+}
+
+void cheat_program_ram_cheat(int index, cht_record_t *cheat) {
+  uint8_t address = 0xc0 + 4 * index;
+  fpga_set_snescmd_addr(address);
+  fpga_write_snescmd(cheat->patchaddr & 0xff);
+  fpga_write_snescmd(cheat->patchaddr >> 8);
+  fpga_write_snescmd(cheat->patchbank);
+  fpga_write_snescmd(cheat->patchvalue);
+  printf("RAM cheat #%d: %02x%04x %02x\n", index, cheat->patchbank, cheat->patchaddr, cheat->patchvalue);
+}
+
+void cheat_load_to_menu(uint8_t *filename, uint32_t address) {
+  printf("cheat_load_to_menu not implemented yet!\n");
+}
+
+void cheat_save_from_menu(uint8_t *filename, uint32_t address) {
+  printf("cheat_save_from_menu not implemented yet!\n");
+}
+
+void cheat_enable(int enable) {
+  uint8_t flags;
+  /* switch ROM cheats */
+  printf("cheat_enable->%d\n", enable);
+  flags = (enable ? 0x01 : 0x10);
+  fpga_write_cheat(7, flags);
+  /* switch WRAM cheats */
+  fpga_set_snescmd_addr(0xbe);
+  fpga_write_snescmd(enable);
+}
+
+void cheat_nmi_enable(int enable) {
+  uint8_t flags;
+  printf("nmi_enable->%d\n", enable);
+  flags = (enable ? 0x02 : 0x20);
+  fpga_write_cheat(7, flags);
+}
+
+void cheat_irq_enable(int enable) {
+  uint8_t flags;
+  printf("irq_enable->%d\n", enable);
+  flags = (enable ? 0x04 : 0x40);
+  fpga_write_cheat(7, flags);
+}
---
+++ src/cheat.h
@@ -0,0 +1,61 @@
+#ifndef CHEAT_H
+#define CHEAT_H
+
+/* sd2snes cheat capabilities:
+ *  -  6 ROM patches
+ *  - 20 WRAM patches
+ *  - in-game button shortcuts to en/disable cheats
+ */
+
+/* menu cheat structure:
+ *  I. 1 byte: number of following cheat records
+ * II. n cheat records according to CHT format
+ */
+
+/* snes9x CHT file format:
+ *  - sequence of cheat data records 28 bytes each
+ *
+ *  offset  size  field
+ *  =================================================================
+ *      0     1   flags (bit 2: cheat disabled;
+ *                       bit 3: cheat contains compare value)
+ *      1     1   patch data value
+ *      2     3   patch data address
+ *      5     1   patch compare value (if flags bit 3 set)
+ *      6     2   unused (set to FE FC by snes9x)
+ *      8    20   cheat description (ASCII)
+ *
+ *  - compare value: only apply (WRAM) cheat if address previously
+ *    contained this value
+ */
+#include <arm/NXP/LPC17xx/LPC17xx.h>
+
+typedef struct _cht_record {
+  uint8_t  flags;
+  uint8_t  patchvalue;
+  uint16_t patchaddr;
+  uint8_t  patchbank;
+  uint8_t  comparevalue;
+  uint16_t pad1;
+  uint8_t  description[20];
+} cht_record_t;
+
+/* deploy cheats to SNES code / FPGA */
+void cheat_program(void);
+
+/* deploy ROM cheat to FPGA */
+void cheat_program_rom_cheat(int index, cht_record_t *cheat);
+
+/* deploy WRAM cheat to SNES code */
+void cheat_program_ram_cheat(int index, cht_record_t *cheat);
+
+/* load CHT file to RAM */
+void cheat_load_to_menu(uint8_t *filename, uint32_t address);
+void cheat_save_from_menu(uint8_t *filename, uint32_t address);
+
+/* enable/disable ROM cheats + hooks */
+void cheat_enable(int enable);
+void cheat_nmi_enable(int enable);
+void cheat_irq_enable(int enable);
+
+#endif
