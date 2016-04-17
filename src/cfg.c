da1aef1ba8565dd6c42a564875e0b70b6076b450
ikari <otakon@gmx.net>
2016-04-17 13:11:23 +0200

    Fix wrong data type for auto region patch setting (value didn't save correctly)

--- src/cfg.c
+++ src/cfg.c
@@ -51,7 +51,7 @@ int cfg_save() {
   f_printf(&file_handle, "%s: %s\n", CFG_BSX_USE_USERTIME, CFG.bsx_use_usertime ? "true" : "false");
   f_printf(&file_handle, "%s: %06lX%08lX\n", CFG_BSX_TIME, (uint32_t)(bcdtime>>32), (uint32_t)(bcdtime & 0xffffffffLL));
   f_puts("\n# Enable PPU region flag patching\n", &file_handle);
-  f_printf(&file_handle, "%s: %d\n", CFG_R213F_OVERRIDE, CFG.r213f_override);
+  f_printf(&file_handle, "%s: %s\n", CFG_R213F_OVERRIDE, CFG.r213f_override ? "true" : "false");
   f_puts("\n# IRQ hook related settings\n", &file_handle);
   f_printf(&file_handle, "#  %s: Overall enable IRQ hooks (required for in-game buttons & WRAM cheats)\n", CFG_ENABLE_IRQ_HOOK);
   f_printf(&file_handle, "#  %s: Enable in-game buttons (en/disable cheats, reset sd2snes...)\n", CFG_ENABLE_IRQ_BUTTONS);
