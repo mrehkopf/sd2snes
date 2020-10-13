ab27994028e5779c55adcdc34ad0966b603ad074
ikari <otakon@gmx.net>
2020-10-14 01:15:34 +0200

    update savestate memory map
    steer clear of memory map used by USB save states

--- src/savestate.h
+++ src/savestate.h
@@ -4,18 +4,18 @@
 
 #include <arm/NXP/LPC17xx/LPC17xx.h>
 
-#define SS_BASEDIR				("/sd2snes/states/")
-#define SS_INPUTFILE			("/sd2snes/savestate_inputs.yml")
-#define SS_FIXESFILE			("/sd2snes/savestate_fixes.yml")
-#define SS_CODE_ADDR			0xFC0000L
-#define SS_REQ_ADDR				0xFC2000L
-#define SS_SAVE_INPUT_ADDR		0xFC2002L
-#define SS_LOAD_INPUT_ADDR		0xFC2004L
-#define SS_DELAY_ADDR			0xFC2014L
-#define SS_SLOTS_ADDR			0xFC2015L
-#define SS_SLOTS_INPUT_ADDR		0xFC2016L
-#define SS_CTRL_ADDR			0xFC2018L
-#define SS_FIXES_ADDR			0xFC2020L
+#define SS_BASEDIR          ("/sd2snes/states/")
+#define SS_INPUTFILE        ("/sd2snes/savestate_inputs.yml")
+#define SS_FIXESFILE        ("/sd2snes/savestate_fixes.yml")
+// #define SS_CODE_ADDR        0xFC0000L
+#define SS_REQ_ADDR         0xFE1000L
+#define SS_SAVE_INPUT_ADDR  0xFE1002L
+#define SS_LOAD_INPUT_ADDR  0xFE1004L
+#define SS_DELAY_ADDR       0xFE100EL
+#define SS_SLOTS_ADDR       0xFE100FL
+#define SS_SLOTS_INPUT_ADDR 0xFE1010L
+#define SS_CTRL_ADDR        0xFE1012L
+#define SS_FIXES_ADDR       0xFE1014L
 
 void savestate_program(void);
 void savestate_set_inputs(void);
