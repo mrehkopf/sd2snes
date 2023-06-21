mrehkopf/sd2snes

commit fffe4992c34c58c221f2f44240d8612e16aa6c03

Fix: wrong video mode in SuperCIC pair mode
In pair mode, CIC data lines are supposed to be outputs from the POV of the MCU.
During operation they could be switched back to inputs by the SysTick handler,
resetting the video mode/region back to NTSC regardless of settings.

src/cic.c
@@ -43,27 +43,29 @@ enum cicstates get_cic_state() {
   } else return CIC_FAIL;
 }
 
+void cic_preinit() {
+  GPIO_MODE_IN(SNES_CIC_D0_REG, SNES_CIC_D0_BIT);
+  GPIO_MODE_IN(SNES_CIC_D1_REG, SNES_CIC_D1_BIT);
+}
+
 void cic_init(int allow_pairmode) {
   GPIO_MODE_OUT(SNES_CIC_PAIR_REG, SNES_CIC_PAIR_BIT);
   OUT_BIT(SNES_CIC_PAIR_REG, SNES_CIC_PAIR_BIT, !allow_pairmode);
-  GPIO_MODE_IN(SNES_CIC_D0_REG, SNES_CIC_D0_BIT);
-  GPIO_MODE_IN(SNES_CIC_D1_REG, SNES_CIC_D1_BIT);
 }
 
 /* prepare GPIOs for pair mode + set initial modes */
 void cic_pair(int init_vmode, int init_d4) {
   cic_videomode(init_vmode);
   cic_d4(init_d4);
-
-  GPIO_MODE_OUT(SNES_CIC_D0_REG, SNES_CIC_D0_BIT);
-  GPIO_MODE_OUT(SNES_CIC_D1_REG, SNES_CIC_D1_BIT);
 }
 
 void cic_videomode(int value) {
   OUT_BIT(SNES_CIC_D0_REG, SNES_CIC_D0_BIT, value);
+  GPIO_MODE_OUT(SNES_CIC_D0_REG, SNES_CIC_D0_BIT);
 }
 
 void cic_d4(int value) {
   OUT_BIT(SNES_CIC_D1_REG, SNES_CIC_D1_BIT, value);
+  GPIO_MODE_OUT(SNES_CIC_D1_REG, SNES_CIC_D1_BIT);
 }
 

src/cic.h
@@ -15,6 +15,7 @@ void print_cic_state(void);
 char *get_cic_statename(enum cicstates state);
 char *get_cic_statefriendlyname(enum cicstates state);
 enum cicstates get_cic_state(void);
+void cic_preinit(void);
 void cic_init(int allow_pairmode);
 
 void cic_pair(int init_vmode, int init_d4);

src/main.c
@@ -120,6 +120,8 @@ int main(void) {
   printf("PCONP=%lx\n", LPC_SC->PCONP);
 #endif
   file_init();
+
+  cic_preinit();
   cic_init(0);
 
   fpga_init();

commit d85c7d2dc49c8fc5d3d26cc1f702604108f44756

Fix CIC shutdown when saving settings on unmodded consoles
Regression caused by fffe499:
When saving the config, to set the video mode, CIC data lines were switched to outputs regardless of whether pair mode was detected or enabled. This caused an unmodded console with original CIC to shut down and require a power cycle.

src/cic.c
@@ -57,15 +57,15 @@ void cic_init(int allow_pairmode) {
 void cic_pair(int init_vmode, int init_d4) {
   cic_videomode(init_vmode);
   cic_d4(init_d4);
+  GPIO_MODE_OUT(SNES_CIC_D0_REG, SNES_CIC_D0_BIT);
+  GPIO_MODE_OUT(SNES_CIC_D1_REG, SNES_CIC_D1_BIT);
 }
 
 void cic_videomode(int value) {
   OUT_BIT(SNES_CIC_D0_REG, SNES_CIC_D0_BIT, value);
-  GPIO_MODE_OUT(SNES_CIC_D0_REG, SNES_CIC_D0_BIT);
 }
 
 void cic_d4(int value) {
   OUT_BIT(SNES_CIC_D1_REG, SNES_CIC_D1_BIT, value);
-  GPIO_MODE_OUT(SNES_CIC_D1_REG, SNES_CIC_D1_BIT);
 }
 