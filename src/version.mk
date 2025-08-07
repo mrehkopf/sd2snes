675d1a361ec50f776ec8ff3619833340a1a02853
ikari <otakon@gmx.net>
2025-08-07 12:04:41 +0200

    Fix version check in version.mk for better shell compatibility

--- src/version.mk
+++ src/version.mk
@@ -14,4 +14,4 @@ endif
 
 .PHONY: version_phony
 .ARG_VERSION: version_phony
-	@if [[ `cat .ARG_VERSION 2>&1` != '$(CONFIG_VERSION)' ]]; then echo -n '$(CONFIG_VERSION)' >.ARG_VERSION; fi
+	@read ARG_VERSION < .ARG_VERSION; if [ $${ARG_VERSION}z != '$(CONFIG_VERSION)'z ]; then echo -n '$(CONFIG_VERSION)' >.ARG_VERSION; fi
